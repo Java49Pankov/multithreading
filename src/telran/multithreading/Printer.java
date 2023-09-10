c4fb3fc5c0af4e9dec6f1daedc59cfb4ddd1695f
Evgeny Pankov <volvovnl123@gmail.com>
2023-09-10 18:06:55 +0300

    HW #44 Solution Scheduled Threads Printing

--- src/telran/multithreading/Printer.java
+++ src/telran/multithreading/Printer.java
@@ -1,38 +1,40 @@
 package telran.multithreading;
 
 public class Printer extends Thread {
-	int printer;
-	Printer printerNext;
-	private int printLength;
-	static int nPortions;
-	static int nNumbers;
+	private static final long SLEEPING_TIME = 100;
+	int printerNumber;
+	Printer next;
+	private int inLine;
+	static int overall;
+	static int nPartitions;
 
-	public Printer(int printer) {
-		this.printer = printer;
-		printLength = nNumbers / nPortions;
+	public Printer(int printerNumber) {
+		this.printerNumber = printerNumber;
+		inLine = overall / nPartitions;
 	}
 
-	public static void setPortions(int nPortions) {
-		Printer.nPortions = nPortions;
+	public void setNext(Printer next) {
+		this.next = next;
 	}
 
-	public static void nRuns(int nNumbers) {
-		Printer.nNumbers = nNumbers;
+	public static void setOverall(int overall) {
+		Printer.overall = overall;
 	}
 
-	public void setNextThread(Printer printerNext) {
-		this.printerNext = printerNext;
+	public static void setPartitions(int nPartitions) {
+		Printer.nPartitions = nPartitions;
 	}
 
 	@Override
 	public void run() {
 		int count = 0;
-		while (count < nPortions) {
+		String line = (" " + printerNumber).repeat(inLine);
+		while (count < nPartitions) {
 			try {
-				sleep(100);
+				join();
 			} catch (InterruptedException e) {
-				System.out.println((" " + printer).repeat(printLength));
-				printerNext.interrupt();
+				System.out.println(line);
+				next.interrupt();
 				count++;
 			}
 		}
--- src/telran/multithreading/PrinterControllerAppl.java
+++ src/telran/multithreading/PrinterControllerAppl.java
@@ -2,27 +2,27 @@ package telran.multithreading;
 
 public class PrinterControllerAppl {
 
-	private static final int N_THREADS = 4;
-	private static final int N_PORTIONS = 10;
+	private static final int N_THREADS = 50;
 	private static final int N_NUMBERS = 100;
+	private static final int N_PARTITIONS = 10;
 
 	public static void main(String[] args) {
-		Printer.setPortions(N_PORTIONS);
-		Printer.nRuns(N_NUMBERS);
+		Printer.setPartitions(N_PARTITIONS);
+		Printer.setOverall(N_NUMBERS);
 		Printer[] printers = new Printer[N_THREADS];
-		runThreads(printers);
+		creatingPrinters(printers);
 		printers[0].interrupt();
-
 	}
 
-	private static void runThreads(Printer[] printers) {
+	private static void creatingPrinters(Printer[] printers) {
 		printers[0] = new Printer(1);
 		for (int i = 1; i < printers.length; i++) {
 			printers[i] = new Printer(i + 1);
-			printers[i - 1].setNextThread(printers[i]);
+			printers[i - 1].setNext(printers[i]);
 			printers[i - 1].start();
 		}
-		printers[printers.length - 1].setNextThread(printers[0]);
+		printers[printers.length - 1].setNext(printers[0]);
 		printers[printers.length - 1].start();
 	}
+
 }
