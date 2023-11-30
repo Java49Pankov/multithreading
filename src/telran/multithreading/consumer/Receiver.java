5cd2c237f8474717e580f928a62f9c32e0c6e5e6
Evgeny Pankov <volvovnl123@gmail.com>
2023-11-30 15:22:45 +0700

    HW #48 Odd / Even Messages distribution

--- src/telran/multithreading/consumer/Receiver.java
+++ src/telran/multithreading/consumer/Receiver.java
@@ -1,26 +1,35 @@
 package telran.multithreading.consumer;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 import telran.multithreading.messaging.MessageBox;
 
 public class Receiver extends Thread {
 	private MessageBox messageBox;
 
-	public Receiver(MessageBox messageBox) {
-		setDaemon(true);
+	public MessageBox getMessageBox() {
+		return messageBox;
+	}
+
+	public void setMessageBox(MessageBox messageBox) {
 		this.messageBox = messageBox;
+	}
+
+	public Receiver(MessageBox messageBox) {
+		setDaemon(true); // FIXME
+		this.messageBox = messageBox;
+	}
+
+	public Receiver() {
+		this(null);
 	}
 
 	@Override
 	public void run() {
-		while (true) {
+		while (true) {// FIXME
 			try {
-
 				String message = messageBox.get();
 				System.out.printf("Thread %d has got message: %s\n", getId(), message);
 			} catch (InterruptedException e) {
-
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
--- src/telran/multithreading/producer/Sender.java
+++ src/telran/multithreading/producer/Sender.java
@@ -3,27 +3,27 @@ package telran.multithreading.producer;
 import telran.multithreading.messaging.MessageBox;
 
 public class Sender extends Thread {
-	private MessageBox messageBox;
+	private MessageBox messageBoxOdd;
+	private MessageBox messageBoxEven;
 	private int nMessages;
 
-	public Sender(MessageBox messageBox, int nMessages) {
-		this.messageBox = messageBox;
+	public Sender(MessageBox messageBoxOdd, MessageBox messageBoxEven, int nMessages) {
+		this.messageBoxOdd = messageBoxOdd;
+		this.messageBoxEven = messageBoxEven;
 		this.nMessages = nMessages;
 	}
 
 	@Override
 	public void run() {
+		MessageBox messageBox = null;
 		for (int i = 1; i <= nMessages; i++) {
 			try {
+				messageBox = i % 2 == 0 ? messageBoxOdd : messageBoxEven;
 				messageBox.put("message" + i);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
 			}
-//			try {
-//				sleep(10);
-//			} catch (InterruptedException e) {
-//			}
 		}
 	}
 
